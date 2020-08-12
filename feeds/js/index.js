d803b49ae093fce49ea7410e98f622b6fd8f5974
quocthinhvo <quocthinhvo0@gmail.com>
2020-08-12 20:46:59 +0700

    Thêm khoảng cách giữa các nút nhấn

--- feeds/js/index.js
+++ feeds/js/index.js
@@ -21,11 +21,10 @@ function init(){
     var listrepos = JSON.parse(listrepos_string);
     // lấy được tên các repo mở
     for (var i = 0; i< listrepos.length; i++){
-        var content = '<div class="card"><div class="card-header"><strong id="name">NameRepoValue</strong> <i id="language" style="float: right;">LanguageRepoValue</i></div><div class="card-body"><p id="des" class="card-text">DesRepoValue</p></div><div class="card-footer"><a id="viewFeeds"><button class="btn btn-primary">Feeds</button></a><a id="viewGithub" href="UrlGitHubValue"><button class="btn btn-primary">GitHub</button></a><button class="btn btn-outline-danger"><i id="star" class="fas fa-star">StarRepoValue</i></button><button class="btn btn-outline-danger"><i id="watch" class="fas fa-eye">WatchRepoValue</i></button><button class="btn btn-outline-danger"><i id="fork" class="fas fa-share">ForkRepoValue</i></button></div></div>';
-        // Đổi tên của repo
+        var content = '<li class="list-group-item list-group-item-action"><div class="card shadow"><div class="card-header"><strong id="name">NameRepoValue</strong><i id="language" style="float: right;">LanguageRepoValue</i></div><div class="card-body"><p id="des" class="card-text">DesRepoValue</p></div><div class="card-footer"><a id="viewFeeds"><button class="btn btn-primary">Feeds</button></a><button style="margin-left: 10px;" class="btn btn-outline-danger"><i id="star" class="fas fa-star">StarRepoValue</i></button><button style="margin-left: 10px;" class="btn btn-outline-danger"><i id="watch" class="fas fa-eye">WatchRepoValue</i></button><button style="margin-left: 10px;" class="btn btn-outline-danger"><i id="fork" class="fas fa-share">ForkRepoValue</i></button></div></div></li>';
         content = content.replace("NameRepoValue", listrepos[i].name);
         content = content.replace("DesRepoValue", listrepos[i].description);
-        content = content.replace("UrlGitHubValue", listrepos[i].html_url);
+        //content = content.replace("UrlGitHubValue", listrepos[i].html_url);
         content = content.replace("StarRepoValue",listrepos[i].stargazers_count);
         content = content.replace("WatchRepoValue", listrepos[i].watchers_count);
         content = content.replace("ForkRepoValue", listrepos[i].forks_count);
