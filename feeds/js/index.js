ad50ba247fb88c9d66b70e3d44f8a4c71482198b
quocthinhvo <quocthinhvo0@gmail.com>
2020-08-12 19:00:59 +0700

    Thiết kế hoàn tất cardview

---
+++ feeds/js/index.js
@@ -0,0 +1,25 @@
+///////////////////////////
+//
+// Hàm này để thêm vào listview
+//
+//////////////////////////
+function addItems(){
+
+}
+
+
+////////////////////
+//
+// Khởi hàm init này chạy đầu tiên sau khi page load xong :v
+//
+//////////////////////////
+function init(){
+    var listrepos_string = GitHub_listRepos();
+    var listrepos = JSON.parse(listrepos_string);
+    // lấy được tên các repo mở
+    for (var i = 0; i< listrepos.length; i++){
+        
+    }
+
+
+}
