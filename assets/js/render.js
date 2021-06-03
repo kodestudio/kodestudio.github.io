8d9ca70ff34a0e40c34d71f09d7fa649e9d0d150
kakangocthien109 <kakangocthien109@gmail.com>
2021-06-03 18:20:57 +0700

    Update link phần footer
    Thay đổi chính
    *Thay đổi link phần Footer (đổi từ deadlink đến các trang).
    *Format document cho gọn gàng.
    
    Co-Authored-By: Quốc Thịnh Võ <46624702+quocthinhvo@users.noreply.github.com>

--- assets/js/render.js
+++ assets/js/render.js
@@ -39,92 +39,89 @@ var headerContent = `
 `;
 
 var footerContent = `
-    <div class="footer-newsletter">
-    <div class="container">
+    
+<div class="footer-newsletter">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-12 text-center">
-        <h4>Nhận tin tức từ chúng tôi ngay hôm nay</h4>
+            <h4>Nhận tin tức từ chúng tôi ngay hôm nay</h4>
         </div>
         <div class="col-lg-6">
-        <form action="" method="post">
-            <input type="email" name="email"><input type="submit" value="Đăng kí">
-        </form>
+            <form action="" method="post">
+                <input type="email" name="email"><input type="submit" value="Đăng kí">
+            </form>
         </div>
     </div>
-    </div>
-    </div>
+</div>
+</div>
 
-    <div class="footer-top">
-    <div class="container">
+<div class="footer-top">
+<div class="container">
     <div class="row gy-4">
         <div class="col-lg-5 col-md-12 footer-info">
-        <a href="index.html" class="logo d-flex align-items-center">
-            <!--<img src="assets/img/logo.png" alt="">-->
-            <span>Kode Space</span>
-        </a>
-        <p>Là tổ chức dành cho những bạn trẻ đam mê về công nghệ thông tin và đây là trang chủ của chúng tôi.</p>
-        <div class="social-links mt-3">
-            <a href="https://www.twitter.com/kodestudio" class="twitter"><i class="bi bi-twitter"></i></a>
-            <a href="https://www.facebook.com/kode.page" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="https://www.instagram.com/kode.page" class="instagram"><i
-                class="bi bi-instagram bx bxl-instagram"></i></a>
-            <a href="https://www.github.com/kodestudio" class="github"><i class="bi bi-github bx bxl-github"></i></a>
-            <a href="https://www.youtube.com/channel/UCNE3g4pFx40HJrnBoHzgJ8A" class="youtube"><i
-                class="bi bi-youtube bx bxl-youtube"></i></a>
-        </div>
+            <a href="index.html" class="logo d-flex align-items-center">
+                <!--<img src="assets/img/logo.png" alt="">-->
+                <span>Kode</span>
+            </a>
+            <p>Là tổ chức dành cho những bạn trẻ đam mê về công nghệ thông tin.</p>
+            <div class="social-links mt-3">
+                <a href="https://www.twitter.com/kodestudio" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="https://www.facebook.com/kode.page" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.instagram.com/kode.page" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
+                <a href="https://www.github.com/kodestudio" class="github"><i class="bi bi-github bx bxl-github"></i></a>
+                <a href="https://www.youtube.com/channel/UCNE3g4pFx40HJrnBoHzgJ8A" class="youtube"><i class="bi bi-youtube bx bxl-youtube"></i></a>
+            </div>
         </div>
 
         <div class="col-lg-2 col-6 footer-links">
-        <h4>Liên kết</h4>
-        <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Trang chủ</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="about.htmk">Về chúng tôi</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">blog.html</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Điều khoản và điều kiện</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Quy định chung</a></li>
-        </ul>
+            <h4>Liên kết</h4>
+            <ul>
+                <li><i class="bi bi-chevron-right"></i> <a href="index.html">Trang chủ</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="about.html">Về chúng tôi</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="blog.html">Blog</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="terms.html">Điều khoản và điều kiện</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="member.html">Thành viên</a></li>
+            </ul>
         </div>
 
         <div class="col-lg-2 col-6 footer-links">
-        <h4>Các liên kết khác</h4>
-        <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Sản phẩm</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Dự án đang thực hiện</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Công việc</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Đóng góp</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Cơ sở dữ liệu</a></li>
-        </ul>
+            <h4>Các liên kết khác</h4>
+            <ul>
+                <li><i class="bi bi-chevron-right"></i> <a href="product.html">Sản phẩm</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="project.html">Dự án</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="job.html">Công việc</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="donate.html">Đóng góp</a></li>
+                <li><i class="bi bi-chevron-right"></i> <a href="https://github.com/kodestudio">Cơ sở dữ liệu</a></li>
+            </ul>
         </div>
 
         <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-        <h4>Thông tin nhanh</h4>
-        <p>
-            Khóm 7 <br>
-            Thị trấn Trần Văn Thời, huyện Trần Văn Thời<br>
-            Cà Mau, Việt Nam <br><br>
-            <strong>Số điện thoại:</strong> +84 <br>
-            <strong>Thư điện tử:</strong> kodestudio.dev@gmail.com<br>
-        </p>
+            <h4>Thông tin nhanh</h4>
+            <p>
+                <b>Địa chỉ:</b><br> Kiệt 7, <br> Thị trấn Trần Văn Thời,<br> huyện Trần Văn Thời,<br> Cà Mau, Việt Nam <br><br>
+                <strong>Số điện thoại:</strong><br> +84 <br><br>
+                <strong>Thư điện tử:</strong><br> contact@kodelang.dev<br>kodestudio.dev@gmail.com
+
+            </p>
 
         </div>
 
     </div>
-    </div>
-    </div>
+</div>
+</div>
 
-    <div class="container">
-    <div class="copyright">
-    &copy; Copyright <strong><span>Kode</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
+<div class="container">
+<div class="copyright">
+    &copy; Copyright <strong><span>Kode</span></strong> 2021. All Rights Reserved.
+</div>
+<div class="credits">
     <!-- All the links in the footer should remain intact. -->
     <!-- You can delete the links only if you purchased the pro version. -->
     <!-- Licensing information: https://bootstrapmade.com/license/ -->
     <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
-    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> and edit with ❤ by <a
-        href="https://www.github.com/kodestudio">@koder</a>
-    </div>
-    </div>
+    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> and edit with ❤ by <a href="https://www.github.com/kodestudio">@koder</a>
+</div>
+</div>
 `;
 
 
